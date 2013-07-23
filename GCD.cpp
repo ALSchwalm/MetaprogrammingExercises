d828cfb1f8a1c6884339cb495cee101030677df1
Adam Schwalm <adamschwalm@gmail.com>
2013-07-23 09:45:38 -0500

    Added comments and removed unnecessary helper

--- GCD.cpp
+++ GCD.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 
-template<int one, int two, bool base>
-struct GCDHelper;
+//Generic template, undefined because it should never be used
+template<unsigned int one, unsigned int two, bool base=false>
+struct GCD;
 
-template<int one, int two>
-struct GCDHelper<one,two,true>
+//Base case, the GCD has been found
+template<unsigned int one, unsigned int two>
+struct GCD<one,two,true>
 {
-  static const int value = two+1;
+  static const unsigned int value = two+1;
 };
 
-template<int one, int two>
-struct GCDHelper<one, two, false> : 
-  public GCDHelper<one, two-1, (one%two==0)>{};
-
-template<int one, int two>
-struct GCD : public GCDHelper<one ,two, false>{};
+//Core of the algorithm, count down from either one of the numbers
+//trying to divide it into the other each time, the inheritance loop
+//is broken when a factor is found.
+template<unsigned int one, unsigned int two>
+struct GCD<one, two, false> : 
+  public GCD<one, two-1, (one%two==0)>{};
 
 int main()
 {
   std::cout << GCD<30, 3>::value << std::endl;   //prints 3
-  std::cout << GCD<17, 19>::value << std::endl;  //prints 1
+  std::cout << GCD<2, 1>::value << std::endl;  //prints 1
   return 0;
 }
 
