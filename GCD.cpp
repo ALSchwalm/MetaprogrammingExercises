6511321f07c9be63338d863dede83b965f2d13ed
Adam Schwalm <adamschwalm@gmail.com>
2014-12-04 20:58:42 +0000

    Clang-format / use constexpr where appropriate

--- GCD.cpp
+++ GCD.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 
-//Generic template, undefined because it should never be used
-template<unsigned int one, unsigned int two, bool base=false>
+// Generic template, undefined because it should never be used
+template <unsigned int one, unsigned int two, bool base = false>
 struct GCD;
 
-//Base case, the GCD has been found
-template<unsigned int one, unsigned int two>
-struct GCD<one,two,true>
-{
-  static const unsigned int value = two+1;
+// Base case, the GCD has been found
+template <unsigned int one, unsigned int two>
+struct GCD<one, two, true> {
+    enum { value = two + 1 };
 };
 
-//Core of the algorithm, count down from either one of the numbers
-//trying to divide it into the other each time, the inheritance loop
-//is broken when a factor is found.
-template<unsigned int one, unsigned int two>
-struct GCD<one, two, false> : 
-  public GCD<one, two-1, (one%two==0)>{};
+// Core of the algorithm, count down from either one of the numbers
+// trying to divide it into the other each time, the inheritance loop
+// is broken when a factor is found.
+template <unsigned int one, unsigned int two>
+struct GCD<one, two, false> : public GCD<one, two - 1, (one % two == 0)> {};
 
-int main()
-{
-  std::cout << GCD<30, 3>::value << std::endl;   //prints 3
-  std::cout << GCD<2, 1>::value << std::endl;  //prints 1
-  return 0;
+int main() {
+    std::cout << GCD<30, 3>::value << std::endl; // prints 3
+    std::cout << GCD<2, 1>::value << std::endl;  // prints 1
+    return 0;
 }
-
--- Primes.cpp
+++ Primes.cpp
@@ -2,106 +2,111 @@
 #include <array>
 #include <type_traits>
 
-//wrapper for ints
-template<unsigned int... ints> struct Ints{};
+// wrapper for ints
+template <unsigned int... ints>
+struct Ints {};
 
-//FactorsHelper generic
-template<typename factorList, unsigned int, unsigned int>
+// FactorsHelper generic
+template <typename factorList, unsigned int, unsigned int>
 struct FactorsHelper;
 
-//Generic Move
-template<typename valueList, unsigned int, unsigned int, bool>
+// Generic Move
+template <typename valueList, unsigned int, unsigned int, bool>
 struct Move;
 
-//The value is a factor, so append it to the list
-//and proceed to the next value
-template<unsigned int... valueList, unsigned int begin, unsigned int possibleFactor>
-struct Move<Ints<valueList...>, begin, possibleFactor, true> :
-    FactorsHelper<Ints<valueList..., possibleFactor>, possibleFactor-1, begin>{};
+// The value is a factor, so append it to the list
+// and proceed to the next value
+template <unsigned int... valueList, unsigned int begin,
+          unsigned int possibleFactor>
+struct Move<Ints<valueList...>, begin, possibleFactor, true>
+    : FactorsHelper<Ints<valueList..., possibleFactor>, possibleFactor - 1,
+                    begin> {};
 
-//The value is not a factor so drop it
-//and proceed to the next value
-template<unsigned int... valueList, unsigned int begin, unsigned int possibleFactor>
-struct Move<Ints<valueList...>, begin, possibleFactor, false> :
-    FactorsHelper<Ints<valueList...>, possibleFactor-1, begin>{};
+// The value is not a factor so drop it
+// and proceed to the next value
+template <unsigned int... valueList, unsigned int begin,
+          unsigned int possibleFactor>
+struct Move<Ints<valueList...>, begin, possibleFactor, false>
+    : FactorsHelper<Ints<valueList...>, possibleFactor - 1, begin> {};
 
-//Base case of factor checking. If the current value is 1, drop it and stop
-template<unsigned int... factorList, unsigned int begin>
-struct FactorsHelper<Ints<factorList...>, 1, begin>{
+// Base case of factor checking. If the current value is 1, drop it and stop
+template <unsigned int... factorList, unsigned int begin>
+struct FactorsHelper<Ints<factorList...>, 1, begin> {
     using value = Ints<factorList...>;
-    static constexpr std::array<unsigned int, sizeof...(factorList)> values = {{factorList...}};
+    static constexpr std::array<unsigned int, sizeof...(factorList)> values = {
+        {factorList...}};
 };
 
-//Performs the bulk of the Factor work. Moves the current value to the factorList
-//if the start value is divisible by it (i.e. if it is a factor)
-template<unsigned int... factorList, unsigned int possibleFactor, unsigned int begin>
-struct FactorsHelper<Ints<factorList...>, possibleFactor, begin> :
-    Move<Ints<factorList...>, begin, possibleFactor, (begin%possibleFactor == 0)>{};
+// Performs the bulk of the Factor work. Moves the current value to the
+// factorList if the start value is divisible by it (i.e. if it is a factor)
+template <unsigned int... factorList, unsigned int possibleFactor,
+          unsigned int begin>
+struct FactorsHelper<Ints<factorList...>, possibleFactor, begin>
+    : Move<Ints<factorList...>, begin, possibleFactor,
+           (begin % possibleFactor == 0)> {};
 
-//determine the factors of a number other than itself and 1
-template<unsigned int begin>
-struct Factors : FactorsHelper<Ints<>, begin/2, begin>{};
+// determine the factors of a number other than itself and 1
+template <unsigned int begin>
+struct Factors : FactorsHelper<Ints<>, begin / 2, begin> {};
 
-
-//Generic case of remove
-template<typename intList, bool>
+// Generic case of remove
+template <typename intList, bool>
 struct Remove;
 
-//Generic case of PrimeHelper
-template<typename intList>
+// Generic case of PrimeHelper
+template <typename intList>
 struct PrimeHelper;
 
-//If the bool is false, then the head of the list is non-prime,
-//so we drop it an check the head-1
-template<unsigned int... intList, unsigned int head>
-struct Remove<Ints<head, intList...>, false> : public
-PrimeHelper<Ints<head-1, intList...>>{};
+// If the bool is false, then the head of the list is non-prime,
+// so we drop it an check the head-1
+template <unsigned int... intList, unsigned int head>
+struct Remove<Ints<head, intList...>, false>
+    : public PrimeHelper<Ints<head - 1, intList...>> {};
 
-//The head of the list is prime, so keep it and check the next value
-template<unsigned int... intList, unsigned int head>
-struct Remove<Ints<head, intList...>, true> : public
-PrimeHelper<Ints<head-1, head, intList...>>{};
+// The head of the list is prime, so keep it and check the next value
+template <unsigned int... intList, unsigned int head>
+struct Remove<Ints<head, intList...>, true>
+    : public PrimeHelper<Ints<head - 1, head, intList...>> {};
 
-//special case for 0
-template<>
+// special case for 0
+template <>
 struct PrimeHelper<Ints<0>> {
     using value = Ints<>;
     static constexpr std::array<unsigned int, 0> values = {};
 };
 
-//special case for 1
-template<>
+// special case for 1
+template <>
 struct PrimeHelper<Ints<1>> {
     using value = Ints<>;
     static constexpr std::array<unsigned int, 0> values = {};
 };
 
-//Base case of PrimeHelper, if the head is two, stop
-template<unsigned int... intList>
-struct PrimeHelper< Ints<2, intList...> > {
+// Base case of PrimeHelper, if the head is two, stop
+template <unsigned int... intList>
+struct PrimeHelper<Ints<2, intList...>> {
     using value = Ints<2, intList...>;
-    static constexpr std::array<unsigned int, sizeof...(intList)+1> values = {{2, intList...}};
+    static constexpr std::array<unsigned int, sizeof...(intList)+1> values = {
+        {2, intList...}};
 };
 
-//Bulk of the prime work. Remove the head from the prime list if its factors are non-empty
-template<unsigned int... intList, unsigned int head>
-struct PrimeHelper<Ints<head, intList...>> :
-    public Remove<Ints<head, intList...>,
-                  std::is_same<typename Factors<head>::value, Ints<>>::value>{};
+// Bulk of the prime work. Remove the head from the prime list if its factors
+// are non-empty
+template <unsigned int... intList, unsigned int head>
+struct PrimeHelper<Ints<head, intList...>>
+    : public Remove<Ints<head, intList...>,
+                    std::is_same<typename Factors<head>::value,
+                                 Ints<>>::value> {};
 
-//Interaface for primes
-template<unsigned int upperBound>
-struct Primes : public PrimeHelper<Ints<upperBound>>{};
+// Interaface for primes
+template <unsigned int upperBound>
+struct Primes : public PrimeHelper<Ints<upperBound>> {};
 
 int main() {
     constexpr auto primes = Primes<20>::values;
     static_assert(primes.size() == 8, "Incorrect number of primes.");
-    static_assert(primes[0] == 2  &&
-                  primes[1] == 3  &&
-                  primes[2] == 5  &&
-                  primes[3] == 7  &&
-                  primes[4] == 11 &&
-                  primes[5] == 13 &&
-                  primes[6] == 17 &&
-                  primes[7] == 19, "Incorrect value of primes");
+    static_assert(primes[0] == 2 && primes[1] == 3 && primes[2] == 5 &&
+                      primes[3] == 7 && primes[4] == 11 && primes[5] == 13 &&
+                      primes[6] == 17 && primes[7] == 19,
+                  "Incorrect value of primes");
 }
